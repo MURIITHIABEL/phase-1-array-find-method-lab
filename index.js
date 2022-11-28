f0cde8a6972c8a213e5fab1a557b730af354bfc6
Abel Muriithi <abelmuriithi04@gmail.com>
2022-11-28 20:27:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
+  
+ 
+  function superbowlWin(record) {
+    let result = record.find(record => record.result === "W")
+    if(result) {
+      return result.year;
+    } else {
+      return undefined;
+    }
+  }
